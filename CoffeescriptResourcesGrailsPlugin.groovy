0df80a9d3e3c597e42aac9000859d5e9e432bffc
Edvinas Bartkus <edvinas@bartkus.lt>
2013-02-02 12:24:46 +0200

    0.3.5.

--- CoffeescriptResourcesGrailsPlugin.groovy
+++ CoffeescriptResourcesGrailsPlugin.groovy
@@ -4,7 +4,7 @@ import org.grails.plugin.resource.ResourceProcessor
 import org.grails.plugin.resource.ResourceTagLib
 
 class CoffeescriptResourcesGrailsPlugin {
-    def version = "0.3.4"
+    def version = "0.3.5"
     def grailsVersion = "1.3.7 > *"
     def dependsOn = [:]
     def pluginExcludes = [
